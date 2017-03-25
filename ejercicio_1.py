10fbae3fa74e11fb1e907e3b69ab23b77faae2a9
vero <vero@localhost.localdomain>
2017-03-25 11:15:09 -0300

    Aca va el primer intento

---
+++ ejercicio_1.py
@@ -0,0 +1,10 @@
+u = raw_input("usuario: ")
+print(u)
+if(u == "vero" ):
+	contras=input("Contra: ")
+	if(contras == 123):
+	   print("Bienvenido")
+	else:
+	    print("su clave no es correcta")    
+else:	
+	 print("usuario no existente")
